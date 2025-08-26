1839ced7f5f96f24019747a896ff777380d8e676
JoeyRubas <joeymrubas@gmail.com>
2025-08-26 12:55:10 -0400

    add to footer

--- src/frontend/src/components/Footer/Footer.tsx
+++ src/frontend/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 const Footer = () => {
   return (
     <footer className="footer" style={{ zIndex: 100 }}>
-      <p>&copy; 2025 Joey Rubas. All rights reserved.</p>
+      <p>&copy; 2025 Joey Rubas. All rights reserved. Built with DotNet and React</p>
     </footer>
   );
 };
